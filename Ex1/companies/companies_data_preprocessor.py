9b9c06e39f8769aea07a427eddce6dc54a5facb2
hitimr <hitimr@gmail.com>
2020-11-05 17:57:27 +0100

    introduced random forest and scoring

--- Ex1/companies/companies_data_preprocessor.py
+++ Ex1/companies/companies_data_preprocessor.py
@@ -4,7 +4,7 @@ from  sklearn import preprocessing
 from sklearn.impute import SimpleImputer
 
 
-def preprocess(df):   
+def preprocess(df): 
     # Formatting 
     df = df.astype('float') 
     data = df.iloc[:,2:-1]
@@ -40,3 +40,30 @@ def drop_cols(df, cutoff):
     df = df.drop(dropped_cols, axis=1)
     print("Dropped", dropped_cols)
     return df
+
+
+def calculate_score(conf_mat):
+    """ Confucsion Matrix:
+    
+                    actual solv.   actual bankrupt
+    pred. solvent                    
+    pred. bancrup                     
+    
+    """
+
+    TP = - 1    # correct prediction of solvend company - we make a bit of money (negative cost)
+    FP = 20     # predicted solvent but company will go bancrupt - we lose a lot of money
+    FN = 1      # predicted bancrupt but company will stay solvent - we could have made a bit of money
+    TN = 0      # correct prediction of company will go bancrupt - we neither make or lose money
+
+    cost_mat = [
+        [TP, FP],
+        [FN, TN]
+    ]
+
+    max_cost = conf_mat.sum()
+    cost = conf_mat[0][0]*cost_mat[0][0] + conf_mat[1][1]*cost_mat[1][1] +\
+        conf_mat[0][1]*cost_mat[0][1] + conf_mat[1][0]*cost_mat[1][0]
+
+    score = 1 - cost/max_cost
+    return score
