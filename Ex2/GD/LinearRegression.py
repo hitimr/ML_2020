c26fbdfaefc077a756aa44d40b4d33e0c59ceae6
hitimr <hitimr@gmail.com>
2020-11-19 23:25:54 +0100

    moar comments

--- Ex2/GD/LinearRegression.py
+++ Ex2/GD/LinearRegression.py
@@ -19,12 +19,17 @@ class LinearRegression():
     """
     Custom Classifier for linear Regression.
 
-    The basic structure and methods used are similar to the models provided by sklearn to ensure compatibility for our model trainer.
-    The actual logic is implemented from scratch except baisc operations such as matrix/vector operations   
+    The basic structure and methods used are similar to the models provided by
+    sklearn to ensure compatibility for our model trainer. The actual logic is
+    implemented from scratch except baisc operations such as matrix/vector
+    operations   
 
-    A note about naming convention:
-        n -> number of samples 
-        m -> number of features
+    fit() will try to work with as many types of arguments as possible (list,
+    DataFrame, arrays, etc) and tries to convert them accordingly. All
+    subsequent calculations require numpy data-structures
+
+    A note about naming convention: n -> number of samples m -> number of
+        features
 
         x -> vector (size = n)
         X -> Matrix (size = n x m)
@@ -37,9 +42,8 @@ class LinearRegression():
     def fit(self, X, y):
         """Fit the model
 
-        Args:
-            X (array-like): Training data of shape (n_samples, n_features)
-            y (array-like): Target values
+        Args: X (array-like): Training data of shape (n_samples, n_features) y
+            (array-like): Target values
         """   
         X = X.transpose()
         y = np.array(y).flatten()
@@ -49,46 +53,42 @@ class LinearRegression():
 
     def rss_vector(self, x, y, w0, w1):
         """Calculate residual sum of squares for x being an array of size n.
-        Original algorithm from lecture slides which was then optimized for performance
-        The implemented algorithm is the one with the best runtime from bechmark_rss_vector.py
+        Original algorithm from lecture slides which was then optimized for
+        performance The implemented algorithm is the one with the best runtime
+        from bechmark_rss_vector.py
 
-        Args:
-            x (np.array): x values
-            y (np.array): y values
-            w0 (float): offset
+        Args: x (np.array): x values y (np.array): y values w0 (float): offset
             w1 (float): slope
 
-        Returns:
-            float: residual
+        Returns: float: residual
         """        
-        return np.dot(y,y) + w1*w1*np.dot(x,x) - 2*w1*np.dot(x,y) + len(x)*w0*w0 - 2*w0*np.sum(y) + 2*w0*w1*sum(x)
+        return np.dot(y,y) + w1*w1*np.dot(x,x) - 2*w1*np.dot(x,y) + len(x)*w0*w0 \
+        - 2*w0*np.sum(y) + 2*w0*w1*sum(x)
 
     def rss(self, X, y, w0, w1):
-        """Calculate the residual sum of squares for X being a Matrix of size n x m
+        """Calculate the residual sum of squares for X being a Matrix of size 
+        n x m
 
-        Args:
-            X (np.matrix): Matrix of x values
-            y (np.array): y values
-            w0 (np.array): offsets (size = m)
-            w1 (np.array): slopes  (size = m)
+        Args: X (np.matrix): Matrix of x values y (np.array): y values w0
+            (np.array): offsets (size = m) w1 (np.array): slopes  (size = m)
 
-        Returns:
-            np.array: vector vectorcontaining m residua
+        Returns: np.array: vector vectorcontaining m residua
         """        
-        return np.asarray([self.rss_vector(X[j].T, y, w0[j], w1[j]) for j in range(len(X))])
+        return np.asarray([
+            self.rss_vector(X[j].T, y, w0[j], w1[j]) 
+            for j in range(len(X))
+            ])
         
 
     def initialize_w(self, X, y):
-        """Initialize w0 (offset) and w1 (slope).
-        Initial values are chosen by finding the smallest and largest x (x_min, x_max)
-        Then we do linear interpolation for f(x_min) and f(x_max)
+        """Initialize w0 (offset) and w1 (slope). Initial values are chosen by
+        finding the smallest and largest x (x_min, x_max) Then we do linear
+        interpolation for f(x_min) and f(x_max)
 
-        Args:
-            X (np.matrix): Matrix of x values
-            y (np.array): y values
+        Args: X (np.matrix): Matrix of x values y (np.array): y values
 
-        Returns:
-            np.array, np.array: w0 (list of offets), w1 (list of slopesslope)
+        Returns: np.array, np.array: w0 (list of offets), w1 (list of
+            slopesslope)
         """        
         self.check_Xy(X, y)
 
@@ -108,9 +108,10 @@ class LinearRegression():
 
 
     def check_Xy(self, X, y):
-        """Check if X and y are suitable arguments for further calculations
-        X must be a Matrix
-        y must be a vector
+        """ Convenience function.
+        Check if X and y are suitable arguments for further calculations
+        X must be a Matrix of size n x m
+        y must be a vector of size n
 
         Args:
             X (): [description]
@@ -124,8 +125,9 @@ class LinearRegression():
 
         assert len(y.shape) == 1    # y must be 1D
         assert len(X.shape) == 2    # X must be 2D
-        assert len(X[0]) == len(y)  # dimensions dont match
-        return True
+        assert len(X[0]) == len(y)  # dimensions must match
+
+
 
 if __name__ == "__main__":
     reg = LinearRegression()
