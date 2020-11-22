6b00fcf14a3ef723ac544099567e163b7d85db77
GenosW <peter.anton.holzner@student.tuwien.ac.at>
2020-11-22 20:34:20 +0100

    parser: mooneyball

--- Ex2/common/DataParser.py
+++ Ex2/common/DataParser.py
@@ -26,3 +26,21 @@ def parse_test_housePrices(splitData=False):
 
     # Split
     return df
+
+def parse_moneyball(splitData=False):
+    """Parse the testing data set "test_housePrices". This data is a copy from
+    the leture of 11.11. PredictingNumericValues p.3
+
+    Args: splitData (bool, optional): If true the data is split into x and y. Otherwise the complete Dataframe will be returnes. Defaults to False.
+
+    Returns: [DataFrame or DataFrame, DataFrame]: The complete DataFrame or x and y
+    """    
+    df = pd.read_csv(cfg.DATASET_MONEYBALL, sep=",")
+
+    if splitData:
+        y = df[cfg.MONEYBALL_TARGET]
+        x = df[cfg.MONEYBALL_FEATURES]
+        return x, y   # return complete Data Frame
+
+    # Split
+    return df
--- Ex2/config.py
+++ Ex2/config.py
@@ -11,6 +11,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) + f"{SYSTEM_PATH_SEP}"
 
 DATASET_TEST_HOUSEPRICES = PROJECT_ROOT + f"test{SYSTEM_PATH_SEP}test_datasets{SYSTEM_PATH_SEP}test_housePrices.csv"
 
+DATASET_MONEYBALL = PROJECT_ROOT + "Datasets" + SYSTEM_PATH_SEP + "Moneyball" + SYSTEM_PATH_SEP + "baseball.csv"
+MONEYBALL_TARGET = "W"
+MONEYBALL_FEATURES = ["Team","League","Year","RS","RA","W","OBP","SLG","BA","Playoffs","RankSeason","RankPlayoffs","G","OOBP","OSLG"]
 
 
 FP_TOLERANCE = 1e-10 # Tolerance for floating point operations for unit tests
