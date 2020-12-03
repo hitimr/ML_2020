2d41e445e99d55c89cb94b30a5ff4af1539ab464
e1525882 <e1525882@student.tuwien.ac.at>
2020-12-03 16:06:13 +0100

    modeltrainer_single_core update

--- Ex2/common/model_trainer_reg.py
+++ Ex2/common/model_trainer_reg.py
@@ -7,15 +7,17 @@ from time import time
 from sklearn.metrics import accuracy_score ,mean_squared_error
 from sklearn.model_selection import train_test_split, learning_curve
 from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score
+from sklearn.model_selection import ShuffleSplit
 
 # New version for regression
 
 class ModelTrainer():
     params = {}
     sklearn_model = object
-    best_result = pd.DataFrame
+    best_result = pd.DataFrame #tupel
 
-    def __init__(self, sklearn_model, params:dict, row_data_data, row_data_target, Varerror = False, Error = False,learning_curve = learning_curve, f_eval=accuracy_score, rmse_eval = mean_squared_error,  CFeature = train_test_split, CV = KFold, LC = learning_curve, thread_cnt=8):
+    def __init__(self, sklearn_model, params:dict, row_data_data, row_data_target, Varerror = False, Error = False,learning_curve = learning_curve, f_eval=r2_score, rmse_eval = mean_squared_error,  CFeature = train_test_split, CV = KFold, LC = learning_curve,CV_shuffle=ShuffleSplit, thread_cnt=8):
         """Initialize the trainer
 
         Args:
@@ -45,6 +47,7 @@ class ModelTrainer():
         self.Varerror = Varerror
         #self.y_row = np.array(y_test).ravel()
         self.f_eval = f_eval
+        self.CV_shuffle = CV_shuffle
         self.rmse_eval = rmse_eval
         self.thread_cnt = thread_cnt
         self.Error = Error
@@ -69,11 +72,19 @@ class ModelTrainer():
 
         # Generate dictionaries of all posible parameter permutations
         keys, values = zip(*self.params.items())
+        print(keys, values)
         self.permutations_dict = [dict(zip(keys, v)) for v in itertools.product(*values)]    
 
         # Run through all models in parallel threads
-        with Pool(self.thread_cnt) as p:
-            result = p.map(self.analyze_model, self.permutations_dict)
+
+        if(self.thread_cnt > 1):
+            with Pool(self.thread_cnt) as p:
+                result = p.map(self.analyze_model, self.permutations_dict)
+        else:
+            result = []
+            for dik in self.permutations_dict:
+                result.append(self.analyze_model(dik))
+      
 
 
         # wrap up results
@@ -102,30 +113,28 @@ class ModelTrainer():
         if(self.Error):
             parameter_set[self.Error] = self.Varerror(self.y_test, y_pred)
 
-
-        #norm_test = np.linalg.norm(self.y_test)
-        #norm_pred = np.linalg.norm(y_pred)
-        #parameter_set["Normed RMSE"] = self.rmse_eval(self.y_test/norm_test, y_pred/norm_pred)
-        # Add remaining scores
-        #for score, func in zip(["accuracy", "f1", "recall", "precision"], [accuracy_score, f1_score, recall_score, precision_score]):
-        #    if score in self._eval_setup.keys():
-        #        parameter_set[score] = func(self.y_test, y_pred, **self._eval_setup[score])
-        #    else:
-        #        if score == "accuracy":
-        #            parameter_set[score] = func(self.y_test, y_pred)
-        #        else:
-        #            parameter_set[score] = func(self.y_test, y_pred, average='macro')
-
         return parameter_set
 
-    def CV_fold(self, k):
-        kf = self.CV(n_splits=k)
+    def CV_fold(self, k, test_size, random_state):
+        kf = self.CV_shuffle(n_splits=k, test_size=test_size, random_state=random_state)
         kf.get_n_splits(self.row_data_data)
 
         for train_index, test_index in kf.split(self.row_data_data):
             self.x_train, self.x_test = self.row_data_data[train_index], self.row_data_data[test_index]
             self.y_train, self.y_test = self.row_data_target[train_index], self.row_data_target[test_index]
     
+
+    def CV_shuffle_split(self, k, test_size, random_state):
+        score_shuffle = []
+        kf = self.CV_shuffle(n_splits=k, test_size=test_size, random_state=random_state)
+        kf.get_n_splits(self.row_data_data)
+
+        for train_index, test_index in kf.split(self.row_data_data):
+            self.x_train, self.x_test = self.row_data_data[train_index], self.row_data_data[test_index]
+            self.y_train, self.y_test = self.row_data_target[train_index], self.row_data_target[test_index]
+            Best_set, Best_score = self.train()
+            score_shuffle.append(Best_score)
+        return score_shuffle
 
     def TTSplit(self, perc = 0.4, r_split = 42):
         self.x_train, self.x_test, self.y_train, self.y_test = self.CFeature(self.row_data_data, self.row_data_target, test_size=perc, random_state=r_split)
@@ -217,6 +226,7 @@ if __name__ == "__main__":
     from sklearn.metrics import accuracy_score
     from sklearn.metrics import r2_score, mean_squared_log_error
     from sklearn.datasets import load_iris
+    from sklearn.model_selection import ShuffleSplit
         
 
     params = {"alpha" : [0.0001]}
@@ -240,12 +250,19 @@ if __name__ == "__main__":
 
     #modeltrainer = ModelTrainer(DecisionTreeRegressor, params, iris_data, iris_target,mean_squared_log_error, "RMLE",  r2_score)
     modeltrainer = ModelTrainer(DecisionTreeRegressor, params, iris_data, iris_target)#, r2_score)
-    modeltrainer.TTSplit(perc = 0.8)
+    modeltrainer.TTSplit(perc = 0.4)
+    modeltrainer.train()
+    res = modeltrainer.result
+    print(res)
     modeltrainer.CV_fold(k = 4)
-
+    modeltrainer.train()
+    res = modeltrainer.result
+    print(res)
+    socore_list = modeltrainer.CV_shuffle_split(k = 4, test_size = 0.4, random_state = 42)
     modeltrainer.train()
     res = modeltrainer.result
     print(res)
     #modeltrainer.plot_learning_curve(title = title, cv = cv)
     #modeltrainer.LC_plot()
     #modeltrainer.save_result("common/iris_result.csv")
+    print("score_list", socore_list)
