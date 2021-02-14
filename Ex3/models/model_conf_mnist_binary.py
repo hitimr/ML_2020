d0eac9112a8cca4b573c28829e025b2c19528617
hitimr <hitimr@gmail.com>
2021-02-14 21:10:26 +0100

    moved bmlp example file to root and renamed to model_creator.
    added settings for easy switching

---
+++ Ex3/models/model_conf_mnist_binary.py
@@ -0,0 +1,39 @@
+import torchvision.transforms as transforms
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+from config import *
+
+# --- Model Architecture
+ARCHITECTURE = "binary_mnist" # MPyC Architecture
+
+# --- Data Loader
+batch_size = 20  # how many samples per batch to load
+valid_size = 0.2 # percentage of training set to use as validation
+num_workers = 0 # number of subprocesses to use for data loading
+
+# --- Data preparation
+# Thresholding
+transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,)),
+        lambda x: x > 0,
+        lambda x: x.float(),
+])
+
+
+# Model Settings
+model_file_name = MODEL_DIR + "mnist_binary.pt"
+
+# Train Settings
+n_epochs = 5 # number of epochs to train the model
+
+
+
+# System Constants
+MNIST_IMG_HEIGHT = 28
+MNIST_IMG_HWIDTH = 28
+MNIST_PIXEL_CNT = MNIST_IMG_HEIGHT * MNIST_IMG_HWIDTH
