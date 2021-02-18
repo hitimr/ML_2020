3fa92fc99d27f60c01b9e5621d5fe80df8ce9726
GenosW <58387550+GenosW@users.noreply.github.com>
2021-02-18 21:01:19 +0100

    profile

--- Ex3/mpc/profile.py
+++ Ex3/mpc/profile.py
@@ -10,6 +10,15 @@ def get_process_memory():
     process = psutil.Process(os.getpid())
     mem_info = process.memory_info()
     return mem_info.rss
+
+def get_gbyte(byte):
+    return byte / 1024**3
+
+def get_mbyte(byte):
+    return byte / 1024**2
+
+def get_kbyte(byte):
+    return byte / 1024
  
  
 def profile(func):
@@ -19,9 +28,18 @@ def profile(func):
         result = func(*args, **kwargs)
         elapsed_time = elapsed_since(start)
         mem_after = get_process_memory()
-        print("{}: memory before: {:,}, after: {:,}, consumed: {:,}; exec time: {}".format(
-            func.__name__,
-            mem_before, mem_after, mem_after - mem_before,
-            elapsed_time))
+        print(f"{func.__name__}: memory before: {get_mbyte(mem_before):.2f}MB, after: {get_mbyte(mem_after):.2f}MB, consumed: {get_mbyte(mem_after - mem_before):.2f}MB; exec time: {elapsed_time}")
         return result
     return wrapper
+
+@profile
+def test():
+    np.arange(0,100000)
+
+if __name__ == "__main__":
+    print("123")
+    import numpy as np
+    profile(np.arange(0,100000))
+
+    test()
+    
